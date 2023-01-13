9b315fc5b2900db56fa106a7c944289205adc695
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2023-01-13 20:47:19 +0600

    Adds data summary of the products

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,14 +1,20 @@
 from django.views import generic
 
-from product.models import Variant
+from product.models import Variant, Product
 
 
 class CreateProductView(generic.TemplateView):
-    template_name = 'products/create.html'
+    template_name = "products/create.html"
 
     def get_context_data(self, **kwargs):
         context = super(CreateProductView, self).get_context_data(**kwargs)
-        variants = Variant.objects.filter(active=True).values('id', 'title')
-        context['product'] = True
-        context['variants'] = list(variants.all())
+        variants = Variant.objects.filter(active=True).values("id", "title")
+        context["product"] = True
+        context["variants"] = list(variants.all())
         return context
+
+
+class ListProductView(generic.ListView):
+    template_name = "products/list.html"
+    model = Product
+    paginate_by = 2
